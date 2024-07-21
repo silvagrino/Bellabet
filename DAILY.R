c17d29f3a89e6f592fb653ae8419987346a8a1a3
silvagrino <148507231+silvagrino@users.noreply.github.com>
2024-07-20 22:08:34 -0400

    Update and rename paragithub2.R to DAILY.R

--- DAILY.R
+++ DAILY.R
@@ -10,36 +10,19 @@ library(ggplot2)
 library(tidyr)
 
 
-dailyActivity_merged
-
-
 ############ VER RESUMENES Y CABEZAS ############
 
 head(dailyActivity_merged)
-
 # COLUMNAS
-
 colnames(dailyActivity_merged)
 colnames(dailyActivity_merged2)
-
 # NUMERO DE ROWS, FILAS
-
 nrow(dailyActivity_merged)
 nrow(dailyActivity_merged2)
-
-# RESUMEN
-
-summary(dailyActivity_merged)
-summary(dailyActivity_merged2)
-
 # FILAS DISTINTAS
-
 n_distinct(dailyActivity_merged)
 n_distinct(dailyActivity_merged2)
-
 ###### Summary ######
-
-
 summary(dailyActivity_merged %>%
           select(-Id, -ActivityDate))
 summary(dailyActivity_merged2 %>%
@@ -54,25 +37,27 @@ sum(is.na(dailyActivity_merged2))
 
 ##sacar columnas innecesarias##
 
-dataActivity_sindistancia <- select(dailyActivity_merged, -LoggedActivitiesDistance, -VeryActiveDistance, -ModeratelyActiveDistance, -LightActiveDistance, -SedentaryActiveDistance)
-dataActivity_sindistancia2 <- select(dailyActivity_merged2, -LoggedActivitiesDistance, -VeryActiveDistance, -ModeratelyActiveDistance, -LightActiveDistance, -SedentaryActiveDistance)
+dataActivity_sindistancia <- select(dailyActivity_merged, -LoggedActivitiesDistance, -VeryActiveDistance, 
+  -ModeratelyActiveDistance, -LightActiveDistance, 
+  -SedentaryActiveDistance, -TotalDistance, -TrackerDistance
+)
+dataActivity_sindistancia2 <- select(dailyActivity_merged2, -LoggedActivitiesDistance, -VeryActiveDistance, 
+  -ModeratelyActiveDistance, -LightActiveDistance, 
+  -SedentaryActiveDistance, -TotalDistance, -TrackerDistance
+)
 
 ## numeros Id por mes ##
-
 n_distinct(dataActivity_sindistancia$Id)
 n_distinct(dataActivity_sindistancia2$Id)
-n_distinct(dataActivity_SD_big$Id)
 
 ## MERGE ##
-
 dataActivity_SD_big <- merge(dataActivity_sindistancia, dataActivity_sindistancia2, all = TRUE)
 
+# Exportar
 write_csv(dataActivity_SD_big, file = "dataActivity_SD_big.csv")
 
 ## numeros Id por mes ##
-
 n_distinct(dataActivity_SD_big$Id)
-
 
 #Exportar cada uno para analizar solo el dataactivity_sindistancia2 porque es el que tiene mas datos #
 
